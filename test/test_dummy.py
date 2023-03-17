9f6c7c93199af8e05a361a5490e0c46690c6831d
Nicholas Amely <92178377+Namlick@users.noreply.github.com>
2023-03-17 11:16:15 -0700

    App name change

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,7 +1,7 @@
 """Template test module."""
 import pytest
-from amiga_package import __version__
-from amiga_package import ops
+from facial_recognition import __version__
+from facial_recognition import ops
 
 # import the internal libs and test
 
