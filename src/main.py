4826cf55feb8cf2069c9ff94644726e643e62adc
Nicholas Amely <92178377+Namlick@users.noreply.github.com>
2023-03-27 10:54:38 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -108,60 +108,60 @@ class CameraApp(App):
                     encodings = face_recognition.face_encodings(img, boxes)
                     names = []
                     	# loop over the facial embeddings
-	                  for encoding in encodings:
-		                  # attempt to match each face in the input image to our known
-		                  # encodings
-		                  matches = face_recognition.compare_faces(data["encodings"],
+	            	for encoding in encodings:
+						# attempt to match each face in the input image to our known
+		            	# encodings
+		            	matches = face_recognition.compare_faces(data["encodings"],
 			                  encoding)
-		                  name = "Unknown" #if face is not recognized, then print Unknown
+		            	name = "Unknown" #if face is not recognized, then print Unknown
 
-		                    # check to see if we have found a match
-		                  if True in matches:
-			                  # find the indexes of all matched faces then initialize a
-			                  # dictionary to count the total number of times each face
-			                  # was matched
-			                  matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			                  counts = {}
+		             	# check to see if we have found a match
+		            	if True in matches:
+			             	# find the indexes of all matched faces then initialize a
+			                # dictionary to count the total number of times each face
+			                # was matched
+			             	matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			                counts = {}
 
-			                  # loop over the matched indexes and maintain a count for
-			                  # each recognized face face
-			                  for i in matchedIdxs:
-				                  name = data["names"][i]
-				                  counts[name] = counts.get(name, 0) + 1
+			                # loop over the matched indexes and maintain a count for
+			                # each recognized face face
+			                for i in matchedIdxs:
+				                name = data["names"][i]
+				                counts[name] = counts.get(name, 0) + 1
 
-			                  # determine the recognized face with the largest number
+			                # determine the recognized face with the largest number
 		                  	# of votes (note: in the event of an unlikely tie Python
-			                  # will select first entry in the dictionary)
-			                  name = max(counts, key=counts.get)
+			                # will select first entry in the dictionary)
+			                name = max(counts, key=counts.get)
 
-			                  #If someone in your dataset is identified, print their name on the screen
-			                  if currentname != name:
-				                  currentname = name
-				                  #if currentname == 'Nick':
-				                  #	Ncounter = Ncounter+1
-				                  #if currentname == 'Lauren':
-				                  #	Lcounter = Lcounter+1
-				                  #print(currentname)
-#			                  if currentname == 'Nick':
-#				                  print(currentname)
-#				                  os.system("say 'Hello Nick'")
-#			                  if currentname == 'Dr. Kulhandjian':
-#				                  print(currentname)
-#				                  os.system("say 'Hello Dr. Kulhandjian'")
+			                #If someone in your dataset is identified, print their name on the screen
+			                if currentname != name:
+				                currentname = name
+				                #if currentname == 'Nick':
+				                #	Ncounter = Ncounter+1
+				                #if currentname == 'Lauren':
+				                #	Lcounter = Lcounter+1
+				                #print(currentname)
+#			                if currentname == 'Nick':
+#				                print(currentname)
+#				                os.system("say 'Hello Nick'")
+#			                if currentname == 'Dr. Kulhandjian':
+#				                print(currentname)
+#				                os.system("say 'Hello Dr. Kulhandjian'")
 
-		                  # update the list of names
-		                  names.append(name)
+		                # update the list of names
+		                names.append(name)
 
-	                  # loop over the recognized faces
-	                  for ((top, right, bottom, left), name) in zip(boxes, names):
-		                  # draw the predicted face name on the image - color is in BGR
-		                  #cv2.Canny(img, 100, 200)
-		                  cv2.rectangle(img, (left, top), (right, bottom),
-			                  (0, 255, 225), 2)
-		                  y = top - 5 if top - 5 > 5 else top + 5
-		                  cv2.rectangle(img, (left, top-20), (int((left+right)/2), top), (0, 255, 255), -1)
-		                  cv2.putText(img, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			                  .5, (0, 0, 0), 2)
+	                # loop over the recognized faces
+	                for ((top, right, bottom, left), name) in zip(boxes, names):
+		                # draw the predicted face name on the image - color is in BGR
+		                #cv2.Canny(img, 100, 200)
+		                cv2.rectangle(img, (left, top), (right, bottom),
+			                (0, 255, 225), 2)
+		                y = top - 5 if top - 5 > 5 else top + 5
+		                cv2.rectangle(img, (left, top-20), (int((left+right)/2), top), (0, 255, 255), -1)
+		                cv2.putText(img, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			                .5, (0, 0, 0), 2)
                      ###########################################
 
                     texture = Texture.create(
